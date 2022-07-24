Replace BN with IN for noticebily better results Add functionality to save and load models either to train further or use Bug fixes related to normalization

fast_style_transfer.py
@@ -1,5 +1,9 @@
 # TODO: make clean model saving functionality
+# TODO: Experiment with contatenating noise with input image
+#       Think about adding this process as part of the forward pass
+#       of the transformation network
 
+import torch
 from torchvision import transforms
 from models import loss_models, transformation_models
 from torch.utils.data import DataLoader
@@ -7,6 +11,7 @@ from torchvision import datasets
 from PIL import Image
 from torch import optim
 from torchvision.utils import save_image
+from torchvision.transforms.functional import pil_to_tensor, resize
 
 device = "mps"
 
@@ -27,6 +32,10 @@ def train(data_loader, model, loss_model, optimizer):
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
+        # autosaving every 2000 training steps
+        if batch * len(X) % 2e3 == 0:
+            torch.save(model.state_dict(), "auto_save.pth")
+
 
 transform = transforms.Compose(
     [
@@ -39,23 +48,35 @@ transform = transforms.Compose(
 
 
 def main():
-    train_dataset = datasets.ImageFolder(root="data", transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+    train_dataset = datasets.ImageFolder(root="data/fiftyk", transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 
     style_img = transform(Image.open("images/starry-night.jpg")).to(device).unsqueeze(0)
     loss_model = loss_models.VGG19Loss(style_img, device=device)
 
-    model = transformation_models.TransformationModel().to(device)
+    transformation_model = transformation_models.TransformationModel().to(device)
 
-    optimizer = optim.Adam(model.parameters())
+    # code to load pretrained model
+    transformation_model.load_state_dict(
+        torch.load("saved-models/test_model_IN.pth", map_location=torch.device("cpu"))
+    )
 
-    # training for two epochs
-    train(train_loader, model, loss_model, optimizer)
-    train(train_loader, model, loss_model, optimizer)
+    optimizer = optim.Adam(transformation_model.parameters())
+
+    # training for one epoch
+    train(train_loader, transformation_model, loss_model, optimizer)
+
+    torch.save(transformation_model.state_dict(), "saved-models/in_test_model.pth")
 
     # testing it on a sample image
-    test_image = transform(Image.open("images/houses.jpg")).to(device).unsqueeze(0)
-    gen_image = model.eval()(test_image).div(255)
+    test_image = resize(
+        pil_to_tensor(Image.open("images/sultan-qaboos-grand-mosque.jpg"))
+        .mul(255.0)
+        .to(device)
+        .unsqueeze(0),
+        400,
+    )
+    gen_image = transformation_model.eval()(test_image).div(255)
 
     # saving image
     save_image(gen_image.squeeze(0), "test.png")

models/transformation_models.py
@@ -8,12 +8,12 @@ class ResidualBlock(nn.Module):
         # experiment with different padding
         self.block = nn.Sequential(
             nn.Conv2d(filters, filters, 3),
-            nn.BatchNorm2d(filters),
+            nn.InstanceNorm2d(filters, affine=True),
             nn.ReLU(),
             nn.Conv2d(filters, filters, 3),
-            nn.BatchNorm2d(filters),
+            nn.InstanceNorm2d(filters, affine=True),
         )
-        self.batch_norm = nn.BatchNorm2d(filters)
+        self.batch_norm = nn.InstanceNorm2d(filters, affine=True)
         self.relu = nn.ReLU()
 
     def forward(self, input):
@@ -33,13 +33,13 @@ class TransformationModel(nn.Module):
             # downsampling layers
             nn.ReflectionPad2d(40),
             nn.Conv2d(3, 32, 9, padding="same"),
-            nn.BatchNorm2d(32),
+            nn.InstanceNorm2d(32, affine=True),
             nn.ReLU(),
             nn.Conv2d(32, 64, 3, stride=2),
-            nn.BatchNorm2d(64),
+            nn.InstanceNorm2d(64, affine=True),
             nn.ReLU(),
             nn.Conv2d(64, 128, 3, stride=2),
-            nn.BatchNorm2d(128),
+            nn.InstanceNorm2d(128, affine=True),
             nn.ReLU(),
             # residual layers
             ResidualBlock(128),
@@ -49,12 +49,13 @@ class TransformationModel(nn.Module):
             ResidualBlock(128),
             # upsampling using fractional convolutions
             nn.ConvTranspose2d(128, 64, 3, stride=2, output_padding=1),
-            nn.BatchNorm2d(64),
+            nn.InstanceNorm2d(64, affine=True),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 32, 3, stride=2, output_padding=1),
-            nn.BatchNorm2d(32),
+            nn.InstanceNorm2d(32, affine=True),
             nn.ReLU(),
             nn.Conv2d(32, 3, 3),
+            nn.InstanceNorm2d(3, affine=True),
         )
         self.sigmoid = nn.Sigmoid()
 