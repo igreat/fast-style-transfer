de0480d590cc6e21352e923d4f165e7500210b35
igreat <goraguy5@gmail.com>
2022-12-30 16:51:56 +0400

    Move styling image code to its own file organize images into folders

--- saved_models/pretrained_models.py
+++ saved_models/pretrained_models.py
@@ -2,6 +2,6 @@
 
 
 PRETRAINED_MODELS: dict[str, str] = {
-    "starry_night": "saved-models/starry_night_pretrained.pth",
-    "rain_princess": "saved-models/rain_princess_pretrained.pth",
+    "starry_night": "saved_models/starry_night_pretrained.pth",
+    "rain_princess": "saved_models/rain_princess_pretrained.pth",
 }
--- stylize_image.py
+++ stylize_image.py
@@ -1,2 +1,47 @@
-# TODO: here is where I'll put the code to stylize an image
+import torch
+from models import loss_models, transformation_models
+from PIL import Image
+from torchvision.utils import save_image
+from torchvision.transforms.functional import pil_to_tensor
 from saved_models.pretrained_models import PRETRAINED_MODELS
+
+device = {torch.has_cuda: "cuda", torch.has_mps: "mps"}.get(True, "cpu")
+
+
+def stylize_image(path_to_image, path_to_model):
+    img = (
+        pil_to_tensor((Image.open(path_to_image)).convert("RGB"))
+        .unsqueeze(0)
+        .float()
+        .to(device)
+        .div(255)
+    )
+
+    transformation_model = transformation_models.TransformationModel().to(device)
+
+    # code to load pretrained model
+    checkpoint = torch.load(path_to_model)
+    transformation_model.load_state_dict(checkpoint["model_state_dict"])
+
+    mean = loss_models.VGG16Loss.MEAN.to(device)
+    std = loss_models.VGG16Loss.STD.to(device)
+
+    gen_image = transformation_model.eval()(img)
+    gen_image = gen_image * std + mean
+    gen_image = gen_image.clamp(0, 1)
+
+    # saving image
+    save_image(gen_image.squeeze(0), "styled_image.png")
+
+
+if __name__ == "__main__":
+
+    image_config = {
+        "path_to_image": "images/houses.jpg",
+        "path_to_model": PRETRAINED_MODELS["rain_princess"],
+    }
+
+    stylize_image(
+        image_config["path_to_image"],
+        image_config["path_to_model"],
+    )
