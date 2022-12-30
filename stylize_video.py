Fix bug hogging memory, where I forget to disable gradient tracking

stylize_video.py
@@ -1,12 +1,10 @@
-from saved_models.pretrained_models import PRETRAINED_MODELS
-
-# load a video file into a numpy array
 import numpy as np
 import cv2
 import torch
 from models import loss_models, transformation_models
 from utils import preprocess_batch, deprocess_batch
 from torchvision.transforms.functional import resize
+from saved_models.pretrained_models import PRETRAINED_MODELS
 
 device = {torch.has_cuda: "cuda", torch.has_mps: "mps"}.get(True, "cpu")
 
@@ -25,7 +23,11 @@ def stylize_video(
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(video.get(cv2.CAP_PROP_FPS))
 
-    frames_to_capture = 80
+    frames_to_capture = frame_count
+
+    print(f"video dimensions: {width}x{height}")
+    print(f"video frame count: {frame_count}")
+    print(f"video fps: {fps}")
 
     # create a numpy array to store the frames
     frames = np.empty((frames_to_capture, height, width, 3), np.dtype("uint8"))
@@ -40,18 +42,14 @@ def stylize_video(
             frame_index += 1
         else:
             # end of video
-            video.release()
+            break
+    video.release()
 
     # convert the frames to torch tensors
     frames = torch.from_numpy(frames).permute(0, 3, 1, 2)
 
-    # resize the frames to have max dimension of 128
-    frames = resize(frames, 256)
-
     # preprocess the frames to what the model expects
     frames = preprocess_batch(frames, loss_models.VGG16Loss)
-
-    # TODO: turn frames into a batched tensor to speed up the process and reduce memory usage
 
     # setting up the model
     transformation_model = transformation_models.TransformationModel().to(device)
@@ -59,16 +57,18 @@ def stylize_video(
     checkpoint = torch.load(path_to_model)
     transformation_model.load_state_dict(checkpoint["model_state_dict"])
 
-    # stylize the frames in batches of 4
+    transformation_model.requires_grad_(False)
+
+    # stylize the frames in batches of 8
     stylized_frames = torch.empty_like(frames)
-    batch_size = 4
+    batch_size = 8
     for i in range(0, frames_to_capture, batch_size):
         # get the batch
         batch = frames[i : i + batch_size].to(device)
         # stylize the batch
         stylized_batch = transformation_model(batch)
 
-        # unpreprocess the batch
+        # depreprocess the batch
         stylized_batch = deprocess_batch(stylized_batch, loss_models.VGG16Loss, device)
 
         # for some reason the transformed image ends up having slightly different dimensions
@@ -102,13 +102,19 @@ def stylize_video(
         out.write(styled_frame)
 
     out.release()
+
     print(f"styled video saved successfully at {path_to_save}")
+
+    # to add the audio back to the video, run this command in the terminal:
+    # ffmpeg -i {path_to_save} -i {path_to_video} -c copy -map 0:v:0 -map 1:a:0 {path_to_save_audio}
 
 
 if __name__ == "__main__":
     # path to the video file
-    path_to_video = "videos/source_videos/aamon.mp4"
+    path_to_video = "videos/source_videos/vid.mp4"
     # path to the pretrained model
     path_to_model = PRETRAINED_MODELS["rain_princess"]
     # stylize the video
-    stylize_video(path_to_video, path_to_model)
+    stylize_video(
+        path_to_video, path_to_model, "videos/generated_videos/stylized_video.mp4"
+    )