338c0945a4a90198a4d696a314b5704d11ea5965
Lorenzo <tunedconsulting@gmail.com>
2023-01-06 11:07:56 +0000

    use dirname

--- saved_models/pretrained_models.py
+++ saved_models/pretrained_models.py
@@ -1,12 +1,14 @@
+import os
 from typing import Dict
 from pathlib import Path
+dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 # since I'll be using the pretrained models a lot in other files, I'll put them here to be imported
 PRETRAINED_MODELS: Dict[str, str] = {
-    "starry_night": Path(__file__) / "starry_night_pretrained.pth",
-    "rain_princess": Path(__file__) / "rain_princess_pretrained.pth",
-    "abstract": Path(__file__) / "abstract_pretrained.pth",
-    "mosaic": Path(__file__) / "mosaic_pretrained.pth",
+    "starry_night": dir_path / "starry_night_pretrained.pth",
+    "rain_princess": dir_path / "rain_princess_pretrained.pth",
+    "abstract": dir_path / "abstract_pretrained.pth",
+    "mosaic": dir_path / "mosaic_pretrained.pth",
 }
 
 # TODO: retrain the abstract model with slightly different image and hyperparameters
