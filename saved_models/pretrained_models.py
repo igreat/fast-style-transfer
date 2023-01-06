9ad9abfd1f29cb3fe7c1ed979b5b34f40ba79e37
Lorenzo <tunedconsulting@gmail.com>
2023-01-06 11:04:42 +0000

    Improve model files discovery

--- saved_models/pretrained_models.py
+++ saved_models/pretrained_models.py
@@ -1,11 +1,12 @@
 from typing import Dict
+from pathlib import Path
 
 # since I'll be using the pretrained models a lot in other files, I'll put them here to be imported
 PRETRAINED_MODELS: Dict[str, str] = {
-    "starry_night": "saved_models/starry_night_pretrained.pth",
-    "rain_princess": "saved_models/rain_princess_pretrained.pth",
-    "abstract": "saved_models/abstract_pretrained.pth",
-    "mosaic": "saved_models/mosaic_pretrained.pth",
+    "starry_night": Path(__file__) / "starry_night_pretrained.pth",
+    "rain_princess": Path(__file__) / "rain_princess_pretrained.pth",
+    "abstract": Path(__file__) / "abstract_pretrained.pth",
+    "mosaic": Path(__file__) / "mosaic_pretrained.pth",
 }
 
 # TODO: retrain the abstract model with slightly different image and hyperparameters
