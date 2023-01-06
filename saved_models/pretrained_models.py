da90158452f9419e6c73ea4051580e6da89b994d
Lorenzo <tunedconsulting@gmail.com>
2023-01-06 10:55:15 +0000

    Fix type declaration

--- saved_models/pretrained_models.py
+++ saved_models/pretrained_models.py
@@ -1,5 +1,7 @@
+from typing import Dict
+
 # since I'll be using the pretrained models a lot in other files, I'll put them here to be imported
-PRETRAINED_MODELS: dict[str, str] = {
+PRETRAINED_MODELS: Dict[str, str] = {
     "starry_night": "saved_models/starry_night_pretrained.pth",
     "rain_princess": "saved_models/rain_princess_pretrained.pth",
     "abstract": "saved_models/abstract_pretrained.pth",
